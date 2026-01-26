2f45485fb5fcffda092650a648472cee2c4c62ce
rlaqhtjd030206 <kbs7125@naver.com>
2026-01-26 22:18:44 +0900

    온커서 확대 구현

--- Assets/Script/Battle Scene/cardsView.cs
+++ Assets/Script/Battle Scene/cardsView.cs
@@ -24,4 +24,17 @@ public class cardsView : MonoBehaviour
         mana.text = card.Mana.ToString();
         imageSR.sprite = card.Image;
     }
+
+    void OnMouseEnter()
+    {
+        wrapper.SetActive(false);
+        Vector3 pos = new(0, 0, 0);
+        CardViewHoverSystem.Instance.Show(Card, pos);
+    }
+
+    void OnMouseExit()
+    {
+        CardViewHoverSystem.Instance.Hide();
+        wrapper.SetActive(true);
+    }
 }
